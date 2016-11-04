b76db439f6356c0dd26458fd16e05eb86b069f84
Sheena Nickerson <sheenanick@gmail.com>
2016-11-04 12:40:26 -0700

    sort answers by number of votes

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  sortBy: ['votes:desc'],
+  sortedAnswers: Ember.computed.sort('answers', 'sortBy'),
   actions: {
     deleteAnswer(question, answer) {
       this.sendAction('deleteAnswer', question, answer);
