d38cb34220a00e8d2acd3347a80394b2c1e44d17
Sheena Nickerson <sheenanick@gmail.com>
2016-11-03 22:10:37 -0700

    move new question component into new route

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,22 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  showNewQuestionForm: false,
   actions: {
-    showNewQuestionForm() {
-      this.set('showNewQuestionForm', true);
-    },
     saveQuestion() {
       var params = {
         content: this.get('question'),
         author: this.get('author'),
         notes: this.get('notes')
       };
-      this.set('showNewQuestionForm', false);
       this.sendAction('saveQuestion', params);
-    },
-    cancel() {
-      this.set('showNewQuestionForm', false);
     }
   }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -5,11 +5,6 @@ export default Ember.Route.extend({
     return this.store.findAll('question');
   },
   actions: {
-    saveQuestion(params) {
-      var newQuestion = this.store.createRecord('question', params);
-      newQuestion.save();
-      this.transitionTo('index');
-    },
     deleteQuestion(question) {
       var answers = question.get('answers').map(function(answer) {
        return answer.destroyRecord();
